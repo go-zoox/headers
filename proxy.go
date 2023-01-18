98c0949a64df749964a5672aa369607802deac43
whatwewant <tobewhatwewant@outlook.com>
2023-01-18 09:53:46 +0800

    fix: lint

--- proxy.go
+++ proxy.go
@@ -7,7 +7,7 @@ const XForwardedFor = "X-Forwarded-For"
 // XForwardedHost identifies the original hostname requested that a client used to connect to your proxy or load balancer.
 const XForwardedHost = "X-Forwarded-Host"
 
-// XForwardedHost identifies the original port requested that a client used to connect to your proxy or load balancer.
+// XForwardedPort identifies the original port requested that a client used to connect to your proxy or load balancer.
 const XForwardedPort = "X-Forwarded-Port"
 
 // XForwardedProto identifies the original protocol (HTTP or HTTPS) requested that a client used to connect to your proxy or load balancer.
