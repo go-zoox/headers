a78139b7f9ecc242375362c343397cc06295b0fc
whatwewant <tobewhatwewant@outlook.com>
2023-01-18 09:45:39 +0800

    chore(proxy): add XForwardedPort

--- proxy.go
+++ proxy.go
@@ -4,8 +4,11 @@ package headers
 // connecting to a web server through an HTTP proxy or a load balancer.
 const XForwardedFor = "X-Forwarded-For"
 
-// XForwardedHost identifies the original host requested that a client used to connect to your proxy or load balancer.
+// XForwardedHost identifies the original hostname requested that a client used to connect to your proxy or load balancer.
 const XForwardedHost = "X-Forwarded-Host"
+
+// XForwardedHost identifies the original port requested that a client used to connect to your proxy or load balancer.
+const XForwardedPort = "X-Forwarded-Port"
 
 // XForwardedProto identifies the original protocol (HTTP or HTTPS) requested that a client used to connect to your proxy or load balancer.
 const XForwardedProto = "X-Forwarded-Proto"
