d4e6a90bc191cc42b15882d03c64a8aac4015975
Zero <tobewhatwewant@gmail.com>
2023-07-13 10:57:44 +0800

    chore: add more security headers for helmet

--- security.go
+++ security.go
@@ -21,8 +21,32 @@ const CrossOriginEmbederPolicy = "Cross-Origin-Embedder-Policy"
 // CrossOriginOpenerPolicy (COOP) prevents other domain from opening/controlling a window.
 const CrossOriginOpenerPolicy = "Cross-Origin-Opener-Policy"
 
+// ContentSecurityPolicyReportOnly (CSP) controls resources the user agent is allowed to load for a given page.
+const ContentSecurityPolicyReportOnly = "Content-Security-Policy-Report-Only"
+
 // CrossOriginResourcePolicy (CORP) prevents other domains from reading the response of the resources to which this header is applied.
 const CrossOriginResourcePolicy = "Cross-Origin-Resource-Policy"
 
 // UpgradeInsecureRequests sends a signal to the server expressing the client's preference for an encrypted and authenticated response, and that it can successfully handle the upgrade-insecure-requests directive.
 const UpgradeInsecureRequests = "Upgrade-Insecure-Requests"
+
+// XContentTypeOptions stops a browser from trying to MIME-sniff the content type and forces it to stick with the declared content-type.
+const XContentTypeOptions = "X-Content-Type-Options"
+
+// ReferrerPolicy controls how much referrer information (sent via the Referer header) should be included with requests.
+const ReferrerPolicy = "Referrer-Policy"
+
+// PermissionsPolicy allows a site to control which features and APIs can be used in the browser.
+const PermissionsPolicy = "Permissions-Policy"
+
+// OriginAgentCluster (OAC) allows a site to control which features and APIs can be used in the browser.
+const OriginAgentCluster = "Origin-Agent-Cluster"
+
+// XDNSPrefetchControl controls DNS prefetching.
+const XDNSPrefetchControl = "X-DNS-Prefetch-Control"
+
+// XDownloadOptions specifies whether the browser should be allowed to open a download dialog.
+const XDownloadOptions = "X-Download-Options"
+
+// XPermittedCrossDomainPolicies defines a policy for fetching content from a cross-domain policy file (crossdomain.xml).
+const XPermittedCrossDomainPolicies = "X-Permitted-Cross-Domain-Policies"
